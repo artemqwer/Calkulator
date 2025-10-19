25264d0169b497a8aaa511825da90b2e4a239723
artemqwer <167970239+artemqwer@users.noreply.github.com>
2025-10-20 01:28:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+let currentInput = '';
+let previousInput = '';
+let operator = '';
+
+function addNumber(number) {
+    currentInput += number;
+    console.log('Додано число', number);
+    console.log('Поточний ввід:', currentInput);
+    updateDisplay();
+
+}
+function setOperator(op) {
+if (currentInput === '') return;
+if (previousInput !== '') {
+    calculate();
+}
+operator = op;
+previousInput = currentInput;
+currentInput = '';
+console.log('Оператор встановлено:', operator);
+console.log('Попередній ввід:', previousInput);
+}
+function calculate() {
+    if (previousInput === '' || currentInput === '' || operator === '') return;
+    
+    const num1 = parseFloat(previousInput);
+    const num2 = parseFloat(currentInput);
+    let result;
+    
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break; 
+        case '*':
+            result = num1 * num2;
+            break;  
+        case '/':
+            if (num2 === 0) {
+                console.log('Помилка: Ділення на нуль');
+                clearDisplay();
+                return;
+            } 
+        default:
+            return;
+    }
+    
+    currentInput = result.toString();
+    previousInput = '';
+    operator = '';
+    updateDisplay();
+    console.log('Результат:', result);
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    updateDisplay();
+    console.log('Екран очищено');       
+}
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.value = currentInput;
+}
